7d53127b69654bc6c28416ca3a7bd3d331857500
Tehelka <zelordking@gmail.com>
2022-01-07 16:55:24 +0100

    correct renaming of file

--- code_coverage/src/calculator.cpp
+++ code_coverage/src/calculator.cpp
@@ -1,4 +1,4 @@
-#include "lib.h"
+#include "calculator.h"
 #include <iostream>
 
 
--- code_coverage/src/main.cpp
+++ code_coverage/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "lib.h"
+#include "calculator.h"
 
 // entry point of the main project
 int main(int argc, char** argv)
